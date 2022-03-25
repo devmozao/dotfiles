175f1ed301d40e25dc3f6c366bd3f71b5cccc71f
Diogo Fonseca <devmozao@outlook.com>
2022-03-25 04:12:32 -0300

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+Import-Module oh-my-posh
+# oh-my-posh prompt init pwsh | Invoke-Expression
+Set-PoshPrompt -Theme montys #agnoster #M365Princess #cloud-native-azure #montys #powerlevel10k_classic #slimfat #sonicboom_dark #free-ukraine
